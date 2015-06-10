1003196a4fe6eeccadfb6b606ae77b208705725f
Jee Hoon <yamoo9@naver.com>
2015-06-10 20:38:09 +0900

    main.js 파일에 require.config() 별칭 추가 및 호출한 모듈 확인

--- DEMO/requireJS/js/main.js
+++ DEMO/requireJS/js/main.js
@@ -8,6 +8,23 @@
  * --------------------------------
  */
 
-require(['libs/jquery-1.11.3.min'], function($) {
-	console.log($ === window.jQuery);
+/**
+ * RequireJS 환경설정 객체
+ * --------------------------------
+ */
+require.config({
+	// 기준 경로 설정
+	baseUrl: 'js/',
+	// 별칭(Alias) 설정
+	paths: {
+		'jquery'    : 'libs/jquery-1.11.3.min',
+		'modernizr' : 'libs/modernizr.custom.71961',
+		'detectizr' : 'libs/detectizr.min'
+	}
+});
+
+// RequireJS 모듈 관리 (의존성관리)
+require(['modernizr', 'jquery'], function(Modernizr, $) {
+	console.log('Modernizr: ' + Modernizr);
+	console.log('jQuery 버전: ' + $.fn.jquery);
 });
