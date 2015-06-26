e87be1d2b519d3cea63cb3cd8444969e46f6b41a
Jee Hoon <yamoo9@naver.com>
2015-06-26 20:57:50 +0900

    jQuery.fn.dataLink 플러그인 완성

--- src/js/init.js
+++ src/js/init.js
@@ -4,5 +4,9 @@ define('init', [
 ],
 function($) {
 	'use strict';
-	console.log('코드 초기화', $ === jQuery);
+	// console.log('코드 초기화', $ === jQuery);
+	$('a').dataLink().on('click', function(e) {
+		e.preventDefault();
+		alert(e.target.getAttribute('href'));
+	});
 });
--- src/js/plugins/jquery.generate.dataLink.js
+++ src/js/plugins/jquery.generate.dataLink.js
@@ -4,5 +4,32 @@ define([
 ],
 function() {
 	'use strict';
-	console.log('사용 중인 jQuery 버전은?\n', $().jquery);
+	// console.log('사용 중인 jQuery 버전은?\n', $().jquery);
+
+	/**
+	 * --------------------------------
+	 * jQuery 플러그인 디자인 패턴
+	 * 이름 정의
+	 * 플러그인 유무 확인
+	 * 플러그인 작성
+	 * 플러그인 내부 this 참조 확인
+	 * 플러그인 체이닝 설정
+	 * --------------------------------
+	 */
+	if ( !$.fn.dataLink ) {
+		$.fn.dataLink = function() {
+			var self = this; // $() 인스턴스
+
+			// 체이닝
+			// return self;
+			return $.each(self, function(index, item) {
+				var $item = self.eq(index);
+				// console.log($(item).attr('href'));
+				// console.log($item.attr('href'));
+				// console.log(item.getAttribute('href').split('http://')[1]);
+				var link_path = $item.attr('href').replace('http://', '');
+				$item.attr('data-link', link_path);
+			});
+		};
+	}
 });
