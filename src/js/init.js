afa7f8b9c35c1ae6a7d26e909a6055b2d8a55741
Jee Hoon <yamoo9@naver.com>
2015-06-29 21:02:46 +0900

    플러그인 쉘 - 일종의 템플릿 (이름, 체이닝, 설정 등등)

--- src/js/init.js
+++ src/js/init.js
@@ -1,10 +1,11 @@
 define('init', [
-	// 'jquery',
-	// 'jquery.dataLink',
-	// 'js/performance-test.js',
 	'jquery.skipNav'
 ],
 function() {
 	'use strict';
+
+	// $.skipNav() 사용법
+	// skipNav()를 적용할 콘테이너 요소 선택자 전달
+	$('#skip-menu').skipNav();
 
 });
--- src/js/plugins/skipNav/jquery.skipNav.js
+++ src/js/plugins/skipNav/jquery.skipNav.js
@@ -1,4 +1,37 @@
 define(['jquery'], function() {
 	'use strict';
 
+	// 플러그인 이름 설정
+	var plugin = 'skipNav';
+
+	// 플러그인 존재 유무 확인
+	if( !$.fn[plugin] ) {
+
+		// 플러그인 정의
+		$.fn[plugin] = function(options) {
+
+			// 플러그인 기본 + 사용자정의 옵션 병합
+			var settings = $.extend({}, $.fn[plugin].defaults, options);
+
+			// 플러그인이 적용된 $() 인스턴스 집합
+			var $this = this;
+
+			// 플러그인 적용 대상이 하나일 때, 체이닝 설정
+			// return this;
+
+			// 플러그인 적용 대상이 하나 이상일 때, 체이닝 설정
+			return $.each($this, function(index, el) {
+				// 플러그인이 적용될 개별 $() 인스턴스 객체(DOM 객체 참조)
+				var $el = $this.eq(index);
+
+			});
+		}
+
+
+		// 플러그인 기본 옵션 설정
+		$.fn[plugin].defaults = {
+
+		};
+	}
+
 });
