08c08541877074add7fd4e3e37251a4557f4f7d9
Jee Hoon <yamoo9@naver.com>
2015-07-01 20:00:05 +0900

    새로운 가상클래스 선택자를 정의 데모

--- src/js/modules/init.js
+++ src/js/modules/init.js
@@ -8,4 +8,23 @@ function() {
 	// skipNav()를 적용할 콘테이너 요소 선택자 전달
 	$('#skip-menu').skipNav();
 
+
+
+
+
+	// 새로운 가상클래스 선택자를 정의
+	// $.expr[':'] === jQuery.expr.pseudos
+	// jQuery.expr.createPseudo()
+
+	$.expr[':'].absolute = function(el) {
+		// return el.style.position === 'absolute';
+		return $.$(el).css('position') === 'absolute';
+	};
+
+	$('div:eq(0)').css('position', 'absolute');
+
+	var divAbs = $('div:absolute()');
+
+	console.log(divAbs);
+
 });
